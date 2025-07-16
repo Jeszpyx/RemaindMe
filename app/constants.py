c9ad80a78cafb8ae5479f1e9debca2bbc3e09a47
Jeszpyx <hleb.lukashonak@yandex.ru>
2025-07-16 19:47:01 +0300

    config env vars

---
+++ app/constants.py
@@ -0,0 +1,19 @@
+from environs import Env
+
+env = Env(expand_vars=True)
+
+env.read_env()
+
+
+class Enviroment:
+
+    def __init__(self):
+        self.BOT_TOKEN = env.str("BOT_TOKEN")
+        self.POSTGRES_USER = env.str("POSTGRES_USER")
+        self.POSTGRES_PASSWORD = env.str("POSTGRES_PASSWORD")
+        self.POSTGRES_DB = env.str("POSTGRES_DB")
+        self.OLLAMA_URL = env.str("OLLAMA_URL")
+        self.OLLAMA_MODEL = env.str("OLLAMA_MODEL")
+
+
+enviroment = Enviroment()
---
+++ app/main.py
@@ -0,0 +1,9 @@
+from constants import enviroment
+
+
+def main():
+    print("Hello from remaindme!", enviroment.OLLAMA_URL)
+
+
+if __name__ == "__main__":
+    main()
